b3c0693442a048227023571f6b3bcfea7b1342d7
Clint Plummer <clint@yuhonas.com>
2023-03-31 10:30:03 -0500

    use base dir as assets are broken during the build

--- site/vite.config.js
+++ site/vite.config.js
@@ -4,7 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// https://vitejs.dev/guide/static-deploy.html
 export default defineConfig({
+  base: '/free-exercise-db/',
   plugins: [vue()],
   resolve: {
     alias: {
