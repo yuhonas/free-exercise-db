45b28d47b96091fcb3258c7cd76a721862c9f40d
Clint Plummer <clint@yuhonas.com>
2023-03-31 15:29:13 -0500

    add favicon's + tweaks

--- site/src/App.vue
+++ site/src/App.vue
@@ -14,6 +14,7 @@ import SearchBar from './components/SearchBar.vue'
                 <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">FEXDB is out! See what's new</span>
                 <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
             </a> -->
+
             <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
               Free Exercise DB
             </h1>
