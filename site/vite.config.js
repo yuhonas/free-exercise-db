f1402d9f5d6f70575322c368c3b4b42f0eccd269
Clint Plummer <clint@yuhonas.com>
2023-03-31 10:39:59 -0500

    configurable base dir so it'll work in development as well

--- site/vite.config.js
+++ site/vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 // https://vitejs.dev/guide/static-deploy.html
 export default defineConfig({
-  base: '/free-exercise-db/',
+  base: process.env['VITE_BASE_DIR'] || '',
   plugins: [vue()],
   resolve: {
     alias: {
