3a3f011b29c9829682259859db1945088f24364f
Clint Plummer <clint@yuhonas.com>
2023-03-31 15:54:12 -0500

    add alt tag + meta description

--- site/src/components/SearchBar.vue
+++ site/src/components/SearchBar.vue
@@ -58,7 +58,7 @@
     </div>
   </form>
   <a v-for="exercise in paginatedItems" href="#" class="flex flex-col mt-4 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <img :src="`./exercises/${exercise.images[0]}`" class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-l" />
+      <img :alt="exercise.name" :src="`./exercises/${exercise.images[0]}`" class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-l" />
       <div class="flex flex-col justify-between p-4 leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{  exercise.name }}</h5>
           <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ exercise.instructions[0] }}</p>
