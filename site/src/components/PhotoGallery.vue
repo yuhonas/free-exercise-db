33df6f066647e2937bc36ffe2fbce365f4c12c81
Clint Plummer <clint@yuhonas.com>
2023-04-20 16:09:42 -0500

    add loading indicator for photo gallery on slow connections we really should bite the bullet and optimize the images

--- site/src/components/PhotoGallery.vue
+++ site/src/components/PhotoGallery.vue
@@ -11,30 +11,45 @@ export default {
   },
   data() {
     return {
-      currentIndex: 0
+      currentIndex: 0,
+      loading: false
     }
   },
   methods: {
+    // NOTE: Assumes we have more then one image
     nextPhoto(event) {
       event.preventDefault()
+      this.loading = true
+
       // cycle through the photos array
       if (this.currentIndex === this.photos.length - 1) {
         this.currentIndex = 0
       } else {
         this.currentIndex++
       }
+    },
+    onImageLoad() {
+      // NOTE: we're really only doing this because the images aren't optimized for size
+      // and it can pause uncomfortably when loading the next image
+      this.loading = false
     }
   }
 }
 </script>
 <template>
-  <div class="relative">
+  <div class="relative" :class="{ loading: loading }">
     <a href="#" @click="nextPhoto">
       <img
         :src="`./exercises/${photos[currentIndex]}`"
         class="w-full object-cover rounded-t-lg p-2"
+        @load="onImageLoad"
       />
     </a>
     <ArrowPathIcon class="w-4 h-3 absolute top-4 left-4 text-white text-sm opacity-80" />
   </div>
 </template>
+<style scoped>
+.loading img {
+  filter: blur(2px);
+}
+</style>
