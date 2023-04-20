fd392fc3365b3d34fa34185de9996afb9492c82b
Clint Plummer <clint@yuhonas.com>
2023-04-20 14:05:25 -0500

    few small styling tweaks to site, truncate long instructions

--- site/src/components/Instructions.vue
+++ site/src/components/Instructions.vue
@@ -18,33 +18,51 @@ export default {
 </script>
 <template>
   <section
-    :class="{ isExpanded: expanded }"
+    :class="{ collapsed: !expanded }"
     v-for="(instruction, index) in text"
     v-bind:key="index"
   >
     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400" v-if="index == 0 || expanded">
       {{ instruction }}
-      <a
-        href="#"
-        @click="expand"
-        v-if="index == 0 && !expanded"
-        class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
-      >
-        More
-        <svg
-          aria-hidden="true"
-          class="w-5 h-5 ml-1"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </a>
     </p>
+    <a
+      href="#"
+      @click="expand"
+      v-if="index == 0 && !expanded"
+      class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+    >
+      More
+      <svg
+        aria-hidden="true"
+        class="w-5 h-5 ml-1"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </a>
   </section>
 </template>
+
+<style scoped>
+section.collapsed p {
+  /**Major Properties**/
+  overflow: hidden;
+  /* line-height: 2rem; */
+  max-height: 6rem;
+  -webkit-box-orient: vertical;
+  display: block;
+  display: -webkit-box;
+  overflow: hidden !important;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 4;
+  /* font-size: 1.5rem; */
+  /* width: 400px; */
+  margin-bottom: 0px;
+}
+</style>
--- site/src/components/SearchBar.vue
+++ site/src/components/SearchBar.vue
@@ -111,9 +111,9 @@ export default {
         <img
           :alt="exercise.name"
           :src="`./exercises/${exercise.images[0]}`"
-          class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-l"
+          class="object-cover w-full ml-2 rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-l"
         />
-        <div class="flex flex-col justify-between p-4 leading-normal">
+        <div class="justify-between p-4 leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {{ exercise.name }}
           </h5>
