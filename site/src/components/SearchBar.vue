7811853ab24b12f18198d09b7d2491545b2cafbf
Clint Plummer <clint@yuhonas.com>
2023-04-20 19:31:49 -0500

    fix broken image dirs as exercise id has changed

--- site/src/components/SearchBar.vue
+++ site/src/components/SearchBar.vue
@@ -30,6 +30,10 @@ export default {
   methods: {
     totalPages() {
       return Math.ceil(this.searchResults.length / this.pageSize)
+    },
+    saveExercise(exercise) {
+      console.log(exercise)
+      this.savedExercises.push(exercise)
     }
   },
   mounted() {
@@ -48,7 +52,7 @@ export default {
   watch: {
     query(oldValue, newValue) {
       const options = {
-        keys: ['name']
+        keys: ['id', 'name']
       }
 
       this.currentPage = 0
@@ -124,6 +128,12 @@ export default {
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {{ exercise.name }}
           </h5>
+          <a
+            href=""
+            @click.prevent="saveExercise(exercise)"
+            class="text-blue-500 hover:text-blue-600 dark:text-blue-400 dark:hover:text-blue-500"
+            >Save</a
+          >
           <Instructions :text="exercise.instructions" />
         </div>
       </div>
--- test.rb
+++ test.rb
@@ -7,11 +7,17 @@ Dir.glob("./exercises/**/*.json") do |file|
 
   # puts json['id']
 
-  id_filename = json["id"] + ".json"
+  json['images'] = json['images'].map do |image|
+    File.join(json['id'], File.basename(image))
+  end
+
+  # puts crap.inspect
+
+  # id_filename = json["id"] + ".json"
   # puts "#{id_filename} = #{File.basename(file)}"
 
-  if id_filename != File.basename(file)
-    puts file
+#   if id_filename != File.basename(file)
+#     puts file
     # target_file = File.dirname(file) + "/" + id_filename
 
     # source_dir = file.chomp(".json")
@@ -22,14 +28,14 @@ Dir.glob("./exercises/**/*.json") do |file|
     # File.rename(file, target_file)
     # puts "#{source_dir} -> #{target_dir}"
     # File.rename(source_dir, target_dir)
-  end
+  # end
 
   # a new key to the JSON file called id that is derived from the filename without the extension
   # json["id"] = File.basename(file, ".json")
 
   # puts json.inspect
   # write the JSON file
-  # File.write(file, JSON.pretty_generate(json))
+  File.write(file, JSON.pretty_generate(json))
 
   # puts file
 
